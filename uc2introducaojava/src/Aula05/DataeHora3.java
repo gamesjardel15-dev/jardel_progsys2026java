16d106a2b50b693a86ddd5c41214bc6e990a145e
L3-3O <leonatan2500@gmail.com>
2026-07-03 11:21:50 -0300

    Aula05

---
+++ uc2introducaojava/src/Aula05/DataeHora3.java
@@ -0,0 +1,14 @@
+package Aula05;
+import java.time.LocalDate;
+public class DataeHora3 {
+
+	public static void main(String[] args) {
+
+        LocalDate dataNascimento = LocalDate.of(2005, 8, 15);
+
+        System.out.println("Data de nascimento: " + dataNascimento);
+        
+        
+	}
+
+}
